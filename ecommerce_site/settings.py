bf9ef6f88bb39a22d1e48b5a8093ec72b5f85bfc
Manojkumar <manojbhosale@users.noreply.github.com>
2024-01-02 22:27:29 +0530

    Update settings.py
    removed password

--- ecommerce_site/settings.py
+++ ecommerce_site/settings.py
@@ -149,4 +149,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'manoj.bhosale31@gmail.com'
-EMAIL_HOST_PASSWORD = 'wxun kgsi sgpq focj'
+EMAIL_HOST_PASSWORD = '<>'
